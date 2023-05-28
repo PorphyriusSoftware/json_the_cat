c6fe5015dc7f032ee4f3f6da6f76ed9e50ff8db2
Porfirio Mendez Ocampo <porfiriomendez76@gmail.com>
2023-05-28 02:12:26 +0000

    tests passing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,24 @@
 const request = require('request');
 
-const args = process.argv.slice(2);
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
-
-
-// console.log(url);
-
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    // console.log(body);
-    const data = JSON.parse(body);
-    // console.log(data);
-    if (data.length > 0) {
-      console.log(data[0].description);
+const fetchBreedDescription = function(breedName, callback) {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  request(url, (error, response, body) => {
+        
+    if (error) {
+      callback(error, null);
+    } else {
+      const gatitos = JSON.parse(body);
+      if (gatitos.length > 0) {
+        callback(error, gatitos[0].description);
+      } else {
+        callback(new Error(`Could not find ${breedName}`), null);
+      }
+      
     }
-  }
-});
+        
+        
+  });
+};
+
+module.exports = { fetchBreedDescription };
